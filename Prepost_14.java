30696d409b68239e5fd647461b3de25dd3908141
SCKboss <=>
2023-05-15 21:26:06 +0700

    prefix postfix

--- Prepost_14.java
+++ Prepost_14.java
@@ -1,5 +1,16 @@
+import java.util.Scanner; 
 public class Prepost_14 {
     public static void main(String[] args) {
-        
+        Scanner sc =new Scanner(System.in); 
+        System.out.println("โปรดใส่ ค่า ของคุณ");
+        int x =sc.nextInt() ;
+        System.out.println("ค่า x = "+x);
+        System.out.println("ค่า ++x = "+(++x));
+        System.out.println("ค่า x++ = "+(x++));
+        System.out.println("ค่า x = "+x);
+        System.out.println("ค่า --x = "+(--x));
+        System.out.println("ค่า x-- = "+(x--));
+        System.out.println("ค่า x = "+x);
+
     }
 }
